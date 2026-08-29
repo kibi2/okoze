3215af68c7309c99596ae8d735e63079d1dc88a3
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-08-29 14:25:13 +0900

    🟢 Nothing happens if the clipboard does not contain an image.

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 document.addEventListener("paste", (event) => {
     const item = event.clipboardData.items[0];
-    const file = item.getAsFile();
-    if (file) {
+    if (item.type.startsWith("image/")) {
+        const file = item.getAsFile();
         const image = new Image();
         image.onload = () => {
             ctx.drawImage(image, 0, 0);
