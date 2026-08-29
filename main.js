1bbdd526c4606cb69bcd354681748c21247605d3
OGAWA Keiji <70509917+kibi2@users.noreply.github.com>
2026-08-29 14:19:06 +0900

    🟢 When "paste" is executed, the clipboard contents are pasted onto the canvas.

--- main.js
+++ main.js
@@ -1,4 +1,13 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
-ctx.fillRect(50, 50, 300, 200);
+document.addEventListener("paste", (event) => {
+    const item = event.clipboardData.items[0];
+    const file = item.getAsFile();
+    if (file) {
+        const image = new Image();
+        image.onload = () => {
+            ctx.drawImage(image, 0, 0);
+        };
+        image.src = URL.createObjectURL(file);
+    }
+});
